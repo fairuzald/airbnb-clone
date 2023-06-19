95d2bd309b379abe414f52880d4a2653040c43fa
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-20 02:02:26 +0700

    Inisialization project use nextjs app routes, tailwind, and typescript

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar:React.FC = () => {
+  return (
+    <div className="fixed w-full bg-white shadow-sm">
+      {/* Container */}
+      <div className=" max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 flex flex-row items-center justify-between gap-3 md:gap-0">
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

a1b762cfeed748766be9ad59b183e7eab7fef184
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-22 03:47:00 +0700

    Resolve hidration rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import getCurrentUser from "./actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
 import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 const font = Nunito({
   subsets: ["latin"],
 });
@@ -23,12 +24,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar currentUser={currentUser} />
-        <LoginModal />
-        <RegisterModal />
-        <Toaster/>
-        <div>{children}</div>
-        <div id="portal"></div>
+        <ClientOnly>
+          <Navbar currentUser={currentUser} />
+          <LoginModal />
+          <RegisterModal />
+          <Toaster />
+          <div>{children}</div>
+          <div id="portal"></div>
+        </ClientOnly>
       </body>
     </html>
   );

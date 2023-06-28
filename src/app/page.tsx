df32b6eb7236760625fb6dac03e1321bc18c3ae7
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-28 23:46:19 +0700

    Try to debug on async page generate(2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,25 @@ import getListings, { IListingsParams } from "./actions/getListings";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/ListingCard";
 interface HomeProps {
-  searchParams: IListingsParams;
-}
-export default async function Home() {
-  // const listings = await getListings();
+  searchParams: IListingsParams
+};
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
-  // if (!listings || listings.length <= 0) {
-  //   return <EmptyState resetButton />;
-  // }
+
+  if (!listings || listings.length <= 0) {
+    return <EmptyState resetButton />;
+  }
 
   return (
     <main className="flex min-h-screen px-18 w-full flex-col pt-[200px] text-black">
       <div className="flex flex-wrap gap-7 w-full items-start justify-center">
-        {/* {listings.map((list) => (
+        {listings.map((list) => (
           <ListingCard key={list.id} data={list} currentUser={currentUser} />
-        ))} */}
+        ))}
       </div>
     </main>
   );
 }
+export default Home
